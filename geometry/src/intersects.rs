ca88f96475dc7e68b69001f779e68c9b2e5a8170
Jan Nils Ferner <jan@myelin.ch>
2019-02-25 19:42:25 +0100

    Add many unicode boxes

--- geometry/src/intersects.rs
+++ geometry/src/intersects.rs
@@ -1,5 +1,25 @@
 /// Tests wether wether this shape touches, contains or is contained in another shape
+/// ```other
+/// ┼─────────────────────────────────────── x
+/// │
+/// │   ┌─────────────┐
+/// │   │             │
+/// │   │          ┌──│───────┐
+/// │   └─────────────┘       │
+/// │              └──────────┘
+/// y
+/// ```
 pub trait Intersects<Other: ?Sized = Self> {
     /// Returns wether this shape touches, contains or is contained in another shape
+    /// ```other
+    /// ┼─────────────────────────────────────── x
+    /// │
+    /// │   ┌─────────────┐
+    /// │   │             │
+    /// │   │          ┌──│───────┐
+    /// │   └─────────────┘       │
+    /// │              └──────────┘
+    /// y
+    /// ```
     fn intersects(&self, other: &Other) -> bool;
 }
